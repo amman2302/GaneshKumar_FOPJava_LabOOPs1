26ed6924ae4ab2bb7001adb5780dead5657ad85a
Ganesh Kumar B <ganesh.vinu@gmail.com>
2022-09-21 00:30:11 +0530

    Create Admin.java

---
+++ com/gl/fssd/itsupport/model/Admin.java
@@ -0,0 +1,8 @@
+package com.gl.fssd.itsupport.model;
+
+public class Admin implements Department {
+    public String getDepartmentName()
+    {
+        return "admin";
+    }//End of getDepartmentName()
+}//End of Admin
